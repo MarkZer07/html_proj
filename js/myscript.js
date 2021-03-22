f90962c109068c5a34afab163e6303bd60d0270c
MarkZer07 <amarq.exp@gmail.com>
2021-03-22 12:46:30 -0300

    Atualizando com os dados do APOD.

---
+++ js/myscript.js
@@ -0,0 +1,4 @@
+document.getElementById('sendmail').addEventListener('click', function (event) {
+            event.preventDefault();
+            document.location.href = 'mailto:amarq.exp@gmail.com';
+        });
