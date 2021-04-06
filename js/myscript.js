33660e86050c88468b9e9a3eef167ff0320da513
MarkZer07 <amarq.exp@gmail.com>
2021-04-05 21:55:08 -0300

    Atualização para a versão 2.

--- js/myscript.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('sendmail').addEventListener('click', function (event) {
-            event.preventDefault();
-            document.location.href = 'mailto:amarq.exp@gmail.com';
-        });
